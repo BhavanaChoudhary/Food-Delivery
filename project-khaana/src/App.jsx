5fd0765b22872538da18a17d31aef7195b374d4f
bhavana <bhavanabc017@gmail.com>
2025-03-23 00:48:00 +0530

    feat: more elements added to home page

--- project-khaana/src/App.jsx
+++ project-khaana/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { StoreContextProvider } from './context/StoreContext';
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Cart from './pages/Cart/Cart';
@@ -8,14 +9,16 @@ import Home from './pages/Home/Home';
 
 const App = () => {
   return (
-    <div className='app'>
+    <StoreContextProvider>
+        <div className='app'>
       <Navbar/>
       <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/order' element={<Placeorder/>}/>
         </Routes>
-    </div>
+        </div>
+    </StoreContextProvider>
 
   )
 }
