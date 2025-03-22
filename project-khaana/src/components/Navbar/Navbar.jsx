6947e316e26af63ec663518c427962726aaf8f40
bhavana <bhavanabc017@gmail.com>
2025-03-22 22:24:43 +0530

    first commit

---
+++ project-khaana/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from "react";
+
+import './Navbar.css'
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search_icon.png';
+import basket_icon from '../../assets/basket_icon.png';
+
+const Navbar = () => {
+  const [menu,setMenu]=useState("home");
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" className="logo"/>
+      <ul className="navbar-menu">
+        <li onClick={()=> setMenu("home")} className={menu=="home"?"active":""}>home</li>
+        <li onClick={()=> setMenu("menu")} className={menu=="menu"?"active":""}>menu</li>
+        <li onClick={()=> setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=> setMenu("contact-us")} className={menu=="contact-us"?"active":""}>contact-us</li>
+        
+      </ul>
+      <div className="navbar-right">
+        <img src={search_icon} ></img>
+        <div className="navbar-search-icon">
+          <img src={basket_icon}></img>
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
