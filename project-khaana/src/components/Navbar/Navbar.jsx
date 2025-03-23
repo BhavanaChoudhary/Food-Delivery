8038c91a7decfa89d65cda8df6e8bf0e4a597b50
bhavana <bhavanabc017@gmail.com>
2025-03-23 18:57:11 +0530

    feat:css and responsiveness

--- project-khaana/src/components/Navbar/Navbar.jsx
+++ project-khaana/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import './Navbar.css'
 import logo from '../../assets/logo.png';
 import search_icon from '../../assets/search_icon.png';
 import basket_icon from '../../assets/basket_icon.png';
 
-const Navbar = () => {
-  const [menu,setMenu]=useState("home");
+const Navbar = ({setShowLogin}) => {
+  const [menu,setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <img src={logo} alt="logo" className="logo"/>
       <ul className="navbar-menu">
-        <li onClick={()=> setMenu("home")} className={menu=="home"?"active":""}>home</li>
-        <li onClick={()=> setMenu("menu")} className={menu=="menu"?"active":""}>menu</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=> setMenu("contact-us")} className={menu=="contact-us"?"active":""}>contact-us</li>
-        
+        <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
+        <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+        <a href="#app-download" onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
+        <a href="#footer" onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact-us</a>
       </ul>
       <div className="navbar-right">
-        <img src={search_icon} ></img>
+        <img src={search_icon} alt="search"/>
         <div className="navbar-search-icon">
-          <img src={basket_icon}></img>
+          <img src={basket_icon} alt="basket"/>
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
